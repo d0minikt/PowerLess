15f6b35b14d46b1736ee3378c87c5357994eb8e7
Dominik Tarnowski <dominik.t.uk@gmail.com>
2018-11-11 12:33:27 +0000

    Improve installation script

--- Install.ps1
+++ Install.ps1
@@ -5,11 +5,13 @@ $PSModulePath = ($Env:PSModulePath -split ";")[0]
 # Add to modules and import whenever a new powershell session is started
 Write-Host "Moving the PowerLess folder to $PSModulePath"
 Move-Item $ThisModule $PSModulePath -Force
-Add-Content -Path $Profile -Value "Import-Module PowerLess -Force;"
+Add-Content -Path $Profile.CurrentUserAllHosts -Value "Import-Module PowerLess -Force;"
 
-# Import module so the user doesn't have to restart the session
+
+
+# Reload the profile so the user doesn't have to restart the session
 try {
-  Import-Module PowerLess
+  . $Profile.CurrentUserAllHosts
   Write-Host "Successfully installed the PowerLess script" -f Blue
 } Catch {
   Write-Error "Couldn't import module. Try following the installation instructions again"
