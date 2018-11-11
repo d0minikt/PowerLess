6c17a1e524478c4f7b6cc4067fc92d3746dd8e14
Dominik Tarnowski <dominik.t.uk@gmail.com>
2018-11-11 10:16:24 +0000

    Improve the installation script, make it more efficient and install for all users

--- Install.ps1
+++ Install.ps1
@@ -1,17 +1,16 @@
 # Get the path to add to the list of Powershell module paths
-$Parent = (Get-Item $PSScriptRoot).Parent.FullName
-Write-Output $Parent
+$ThisModule = (Get-Item $PSScriptRoot).FullName
+$PSModulePath = ($Env:PSModulePath -split ";")[0]
 
 # Add to modules and import whenever a new powershell session is started
-Add-Content -Path $Profile.CurrentUserAllHosts -Value ('$Env:PSModulePath = $Env:PSModulePath+";'+$Parent+'";')
-Add-Content -Path $Profile.CurrentUserAllHosts -Value "Import-Module PowerLess;"
+Write-Host "Moving the PowerLess folder to $PSModulePath"
+Move-Item $ThisModule $PSModulePath -ErrorAction SilentlyContinue
+Add-Content -Path $Profile -Value "Import-Module PowerLess;"
 
-# Refresh the path so the user doesn't have to restart the session
-$ENV:PSModulePath = ($ENV:PSModulePath+";"+$Parent)
-
+# Import module so the user doesn't have to restart the session
 try {
   Import-Module PowerLess
-  Write-Host "Successfully installed PowerLess script" -f Blue
-} finally {
+  Write-Host "Successfully installed the PowerLess script" -f Blue
+} Catch {
   Write-Error "Couldn't import module. Try following the installation instructions again"
 }
